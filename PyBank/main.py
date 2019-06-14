68ba273a7deaa92ea4ca1f74c90b920cb19de870
Chris Marchetti <cmarche2ti@gmail.com>
2019-06-14 11:51:01 -0600

    Fixed max & min difference

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,43 +1,59 @@
+#import dependencies
 import csv
 import os
 
+#initialize variables
 file_path = os.path.join("Resources", "budget_data.csv")
 month_counter = 0
-total_profit = 0
-max_profit = 0
-min_profit = 0
+max_profit_diff = 0
+min_profit_diff = 0
 max_date = ""
 min_date = ""
-#comment here
+profits = []
+profit_diff = []
+dates = []
+
+#open csv file to begin processing
 with open(file_path, "r") as f:
     reader = csv.reader(f)
-    next(reader)
-    for row in reader:
-        month_counter += 1
-        profit = int(row[1])
-        total_profit += profit
-        if profit > max_profit:
-            max_profit = profit
-            max_date = row[0]
-        if profit < min_profit:
-            min_profit = profit
-            min_date = row[0]
+    next(reader) #skip the header row
+    for row in reader:  #read the rows to calculate total months and populate lists
+        month_counter += 1     
+        profits.append(int(row[1]))
+        dates.append(row[0])             
     
+    #create list of difference in profits to get max, min and average
+    diff_profits_list = list(zip(profits, profits[1:]))        
+    for x,y in diff_profits_list:
+        profit_diff.append(y - x)
+
+    for value in profit_diff:    
+        current_profit_diff = value
+        if current_profit_diff < min_profit_diff:  
+            min_profit_diff = current_profit_diff
+        if current_profit_diff > max_profit_diff:  
+            max_profit_diff = current_profit_diff     
+    
+    max_date = dates[profit_diff.index(max_profit_diff) + 1]
+    print(max_date)
+    min_date = dates[profit_diff.index(min_profit_diff) + 1]
+    #Display results in terminal
     print("Financial Analysis")
     print("-" * 30)
     print(f"Total Months: {month_counter}")
-    print(f"Total: {total_profit}")
-    print(f"Average Change: {total_profit / month_counter}")
-    print(f"Greatest Increase in Profits: {max_date} ({max_profit})")
-    print(f"Greatest Decrease in Profits: {min_date} ({min_profit})")
+    print(f"Total: ${sum(profits)}")
+    print(f"Average Change: ${sum(profit_diff) / len(profit_diff):.2f}")
+    print(f"Greatest Increase in Profits: {max_date} (${max(profit_diff)})")
+    print(f"Greatest Decrease in Profits: {min_date} (${min(profit_diff)})")
 
+    #write results to a text file
     with open('profit_summary.txt', 'w') as f:
         f.write("Financial Analysis \n")
         f.write("---------------------------\n")
         f.write(f"Total Months: {month_counter} \n")
-        f.write(f"Total: {total_profit} \n")
-        f.write(f"Average Change: {total_profit / month_counter} \n")
-        f.write(f"Greatest Increase in Profits: {max_date} ({max_profit}) \n")
-        f.write(f"Greatest Decrease in Profits: {min_date} ({min_profit}) \n")
+        f.write(f"Total: ${sum(profits)} \n")
+        f.write(f"Average Change: ${sum(profit_diff) / len(profit_diff):.2f} \n")
+        f.write(f"Greatest Increase in Profits: {max_date} (${max(profit_diff)}) \n")
+        f.write(f"Greatest Decrease in Profits: {min_date} (${min(profit_diff)} \n")
     
 
